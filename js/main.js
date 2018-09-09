da69c5ef7b404702b079aad6387a014261208f0d
Chase Carnaroli <chasecarnaroli@gmail.com>
2018-09-08 19:13:38 -0700

    Fix navbar jump glitch

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
 /***************************
     Global Variables
 ***************************/
-var navbar;
-var navbarOffset;
-
-window.onload = function(){
-    navbar = document.getElementById("nav_bar");
-}
-
-/***************************
-    Sticky Nav Bar
-***************************/
-window.onscroll = function(){
-    keepNavBarAtTop()
-};
-
-function keepNavBarAtTop() {
-    navbar.classList.add("sticky")
-}
